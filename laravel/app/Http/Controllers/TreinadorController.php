b08d154bb253e1e4eb2000de19151b9bfc741a5d
rafasilv-cyber <rafaelgames2609@gmail.com>
2026-09-18 22:38:14 -0300

    ini

---
+++ laravel/app/Http/Controllers/TreinadorController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Treinador;
+use Illuminate\Http\Request;
+
+class TreinadorController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Treinador $treinador)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Treinador $treinador)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Treinador $treinador)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Treinador $treinador)
+    {
+        //
+    }
+}
